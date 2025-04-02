dbb624570c4145e44abb269b297a154826045722
DESKTOP-CB154K1\nagai <nagadon0424@icloud.com>
2025-04-03 03:51:14 +0900

    手直し

--- NewFlappyBird/Assets/Scripts/PipeSpawner.cs
+++ NewFlappyBird/Assets/Scripts/PipeSpawner.cs
@@ -10,6 +10,7 @@ namespace FlappyBird
         [SerializeField] private float maxHeight = 2f;
         [SerializeField] private float xSpawnPosition = 10f;
         [SerializeField] private float gapSize = 4f; // パイプ間の隙間サイズ
+        [SerializeField] private float pipeLength = 30f; // パイプの長さ（上下それぞれ）
 
         private float timer;
 
@@ -30,12 +31,12 @@ namespace FlappyBird
             float randomHeight = Random.Range(minHeight, maxHeight);
             
             // 下のパイプを生成
-            GameObject lowerPipe = Instantiate(pipePrefab, new Vector3(xSpawnPosition, randomHeight - (gapSize/2), 0), Quaternion.identity);
-            lowerPipe.transform.localScale = new Vector3(1f, 8f, 1f); // パイプの高さを8倍に
+            GameObject lowerPipe = Instantiate(pipePrefab, new Vector3(xSpawnPosition, randomHeight - (gapSize/2) - (pipeLength/10), 0), Quaternion.identity);
+            lowerPipe.transform.localScale = new Vector3(3f, pipeLength, 1f);
             
             // 上のパイプを生成
-            GameObject upperPipe = Instantiate(pipePrefab, new Vector3(xSpawnPosition, randomHeight + (gapSize/2), 0), Quaternion.identity);
-            upperPipe.transform.localScale = new Vector3(1f, 8f, 1f); // パイプの高さを8倍に
+            GameObject upperPipe = Instantiate(pipePrefab, new Vector3(xSpawnPosition, randomHeight + (gapSize/2) + (pipeLength/10), 0), Quaternion.identity);
+            upperPipe.transform.localScale = new Vector3(3f, pipeLength, 1f);
             
             // パイプをまとめるための親オブジェクトを作成
             GameObject pipeGroup = new GameObject("PipeGroup");
